775946eff6846de035a476fdbf1d9ed5c1289194
GuglielmoBini <guglielmobini@gmail.com>
2023-03-21 19:54:10 +0100

    made route to receive messages

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\VideogameController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,6 @@ Route::get('/videogames', [VideogameController::class, 'index']);
 
 // Route to api show videogames
 Route::get('/videogames/{videogame}', [VideogameController::class, 'show']);
+
+// route to receive messages from users
+Route::post('/contact-message', [ContactController::class, 'send']);
