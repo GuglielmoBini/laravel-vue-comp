ef4ee88db9d52cdcd2becf8d316c6d39ca44e408
Alberto-Bruno <brunoalberto97@outlook.it>
2023-03-22 17:45:42 +0100

    add Fontawesome add genre

--- resources/views/admin/videogames/index.blade.php
+++ resources/views/admin/videogames/index.blade.php
@@ -6,9 +6,9 @@
                 <th class="text-primary">#id</th>
                 <th class="text-primary">Title</th>
                 <th class="text-primary">Platform</th>
+                <th class="text-primary">Genre</th>
                 <th class="text-center text-primary">Created at</th>
                 <th class="text-center text-primary">Updated at</th>
-                <th class="text-end text-primary">Actions</th>
             </thead>
             <tbody>
                 @forelse($videogames as $videogame)
@@ -16,13 +16,14 @@
                         <td>{{ $videogame->id }}</td>
                         <td>{{ $videogame->title }}</td>
                         <td>{{ $videogame->platform }}</td>
-    
+                        <td>{{ $videogame->genre }}</td>
+
                         <td class="text-end">
                             <a href="{{ route('admin.videogames.show', $videogame->id) }}"
                                 class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-eye"></i></a>
                             <a href="{{ route('admin.videogames.edit', $videogame->id) }}"
-                                class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-pencil"></i></a>                               
-                        </td> 
+                                class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-pencil"></i></a>
+                        </td>
                     </tr>
                 @empty
                     <tr>
@@ -31,5 +32,7 @@
                 @endforelse
             </tbody>
         </table>
+        <a href="{{ route('admin.videogames.create') }}" class="btn btn-outline-primary"><i
+                class="fa-solid fa-upload me-2"></i>Upload game</a>
     </div>
 @endsection
